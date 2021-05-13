e115a1f0afa9b5acd6df84a6d819bfe2f80ea345
Magomed <50445215+Fegustin@users.noreply.github.com>
2021-05-13 17:59:24 +0300

    Update Keyboard.kt

--- app/src/main/java/ru/steilsouth/ellcom/utils/Keyboard.kt
+++ app/src/main/java/ru/steilsouth/ellcom/utils/Keyboard.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
+// Скрытие клавиатуры
 fun View.hideKeyboard() {
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
     imm.hideSoftInputFromWindow(windowToken, 0)
