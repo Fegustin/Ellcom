8af91adb48a2010ca1c2d7cc28a4033f7afe15c2
Magomed <50445215+Fegustin@users.noreply.github.com>
2021-05-13 17:59:49 +0300

    Update Mask.kt

--- app/src/main/java/ru/steilsouth/ellcom/utils/Mask.kt
+++ app/src/main/java/ru/steilsouth/ellcom/utils/Mask.kt
@@ -3,6 +3,7 @@ package ru.steilsouth.ellcom.utils
 import android.widget.EditText
 import com.redmadrobot.inputmask.MaskedTextChangedListener
 
+// Установка маски для editText
 fun setFormat(format: String, editText: EditText) {
     val listener = MaskedTextChangedListener(format, editText)
     editText.addTextChangedListener(listener)
